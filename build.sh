84d69ce7db0b8d4f5bfc57e1d5c00d8f6c05ded0
Christian Cleberg <hello@cleberg.net>
2024-04-19 21:47:59 -0500

    fix location of security.txt and update deployment method

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ if [ "$ENV" == "prod" ]; then
 	echo "Environment = Production"              && \
 	rm -rf .build/*                              && \
 	emacs --script publish.el                    && \
-	scp -r .build/* ubuntu:/var/www/cleberg.net/
+	rsync -avzP .build/ ubuntu:/var/www/cleberg.net/
 else
 	echo "Environment = Development"             && \
 	rm -rf .build/*                              && \
