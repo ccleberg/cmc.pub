68b9918176673eecc9c357e3a9d7485a1950917c
Christian Cleberg <hello@cleberg.net>
2024-05-25 19:19:25 -0500

    fix build script

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 
 if [ "$ENV" == "prod" ]; then
 	echo "Environment: Production"               && \
-	rm -rf .build/*                              && \
+	rm -rf public/*                              && \
 	zola build                                   && \
 	rsync -avzP public/ ubuntu:/var/www/cleberg.net/
 else
 	echo "Environment: Development"              && \
-	rm -rf .build/*                              && \
+	rm -rf public/*                              && \
 	zola build                                   && \
 	zola serve
 fi
