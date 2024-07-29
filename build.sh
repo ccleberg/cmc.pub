79b16809e66d69651979215055ed808171769e43
Christian Cleberg <hello@cleberg.net>
2024-07-28 21:22:23 -0500

    suppress excessive build script output

--- build.sh
+++ build.sh
@@ -5,20 +5,21 @@ read answer
 
 if [ "$answer" != "${answer#[Yy]}" ] ; then
 	if [ "$ENV" == "prod" ]; then
-		echo "Environment = Production"                  && \
+		echo "Environment: Production"                   && \
 		rm -rf .build/*                                  && \
-		emacs --script publish.el			 && \
+		emacs --script publish.el &>/dev/null		 && \
 		pandoc -f org -t html readme.org  -o readme.html && \
 		hut git update --readme readme.html --repo          \
 		https://git.sr.ht/\~cyborg/cleberg.net           && \
-		rsync -av --delete-before .build/* ubuntu:/var/www/cleberg.net/
+		rsync -r --delete-before .build/*		    \
+		ubuntu:/var/www/cleberg.net/
 	else
-		echo "Environment = Development"             && \
+		echo "Environment: Development"              && \
 		rm -rf .build/*                              && \
 		emacs --script publish.el		     && \
 		cd .build/				     && \
 		python3 -m http.server
 	fi
 else
-	echo No
+	echo "Please update the 'Recent Blog Posts' section before publishing!"
 fi
