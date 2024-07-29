afe76ac7d7498b862abaa623790b91410e34574d
Christian Cleberg <hello@cleberg.net>
2024-07-28 19:45:52 -0500

    add safety check to build.sh

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+printf "Did you update the 'Recent Blog Posts' section? [yn]"
+read answer
+
+if [ "$answer" != "${answer#[Yy]}" ] ; then
+	if [ "$ENV" == "prod" ]; then
+		echo "Environment = Production"              && \
+		rm -rf .build/*                              && \
+		emacs --script publish.el                    && \
+		rsync -av --delete-before public/* ubuntu:/var/www/cleberg.net/
+	else
+		echo "Environment = Development"             && \
+		rm -rf .build/*                              && \
+		emacs --script publish.el		     && \
+		cd .build/				     && \
+		python3 -m http.server
+	fi
+else
+	echo No
+fi
