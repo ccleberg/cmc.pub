bc5e26d73dc0da6cfba8046f302e540c1d0f4497
Christian Cleberg <hello@cleberg.net>
2024-07-28 21:53:25 -0500

    add support for filetags

--- build.sh
+++ build.sh
@@ -8,6 +8,8 @@ if [ "$answer" != "${answer#[Yy]}" ] ; then
 		echo "Environment: Production"                   && \
 		rm -rf .build/*                                  && \
 		emacs --script publish.el &>/dev/null		 && \
+		minify -o theme/static/styles.min.css		    \
+		theme/static/styles.css				 && \
 		pandoc -f org -t html readme.org  -o readme.html && \
 		hut git update --readme readme.html --repo          \
 		https://git.sr.ht/\~cyborg/cleberg.net           && \
@@ -17,6 +19,8 @@ if [ "$answer" != "${answer#[Yy]}" ] ; then
 		echo "Environment: Development"              && \
 		rm -rf .build/*                              && \
 		emacs --script publish.el		     && \
+		minify -o theme/static/styles.min.css		\
+		theme/static/styles.css			     && \
 		cd .build/				     && \
 		python3 -m http.server
 	fi
