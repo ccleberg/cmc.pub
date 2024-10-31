885d333a306bec57d798ec73cf93225bc2dc0fb5
Christian Cleberg <hello@cleberg.net>
2024-10-31 09:57:41 -0500

    enhance build script

--- build.sh
+++ build.sh
@@ -3,22 +3,19 @@
 printf "Did you update the 'Recent Blog Posts' section? [yn] "
 read answer
 
-if [ "$answer" != "${answer#[Yy]}" ] ; then
-	if [ "$ENV" == "prod" ]; then
-		echo "Environment: Production"                   && \
-		rm -rf .build/*                                  && \
-		emacs --script publish.el &>/dev/null		 && \
-		minify -o theme/static/styles.min.css		    \
-		theme/static/styles.css				 && \
-		rsync -r --delete-before .build/*		    \
-		ubuntu:/var/www/cleberg.net/
+if [[ "$answer" =~ ^[Yy]$ ]]; then
+    if [[ "$ENV" == "prod" ]]; then
+    echo "Environment: Production"
+    rm -rf .build/*
+    emacs --script publish.el &>/dev/null
+    minify -o theme/static/styles.min.css theme/static/styles.css
+    rsync -r --delete-before .build/* ubuntu:/var/www/cleberg.net/
 	else
-		echo "Environment: Development"              && \
-		rm -rf .build/*                              && \
-		emacs --script publish.el		     && \
-		minify -o theme/static/styles.min.css		\
-		theme/static/styles.css			     && \
-		cd .build/				     && \
+    echo "Environment: Development"
+    rm -rf .build/*
+    emacs --script publish.el
+    minify -o theme/static/styles.min.css theme/static/styles.css
+    cd .build/
 		python3 -m http.server
 	fi
 else
