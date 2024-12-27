1baf98a81e7e0f4d903419b90469ec92b153edab
Christian Cleberg <hello@cleberg.net>
2024-12-27 10:20:15 -0600

    update git links

--- build.sh
+++ build.sh
@@ -32,10 +32,6 @@ if [[ "$answer" =~ ^[Yy]$ ]]; then
         
         # Minify CSS
         minify -o theme/static/styles.min.css theme/static/styles.css
-
-        # Update README on Sourcehut
-        pandoc -f org -t html readme.org  -o readme.html
-		hut git update --readme readme.html --repo https://git.sr.ht/\~cyborg/cleberg.net
         
         # Deploy changes
         rsync -r --delete-before .build/* $ubuntu_server:/var/www/cleberg.net/
